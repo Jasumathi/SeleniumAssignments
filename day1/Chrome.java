f88e1d635e4664ff3f736f92ea786a5e3ad3f162
Jasumathi <jasumathisuresh@gmail.com>
2025-11-09 08:36:22 +0530

    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String	developer= "Google";
+		boolean isBeta= false;
+		int releaseYear= 2008;
+		char shortcutKey= 'C';
+		System.out.println("Version is "+version);
+		System.out.println("Developer Company name is "+developer);
+		System.out.println("Is Beta Type is "+isBeta);
+		System.out.println("Release Year is "+releaseYear);
+		System.out.println("ShortcutKey for Char is "+shortcutKey);
+		
+	}
+
+}
